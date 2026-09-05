8ba37f293d2038664fccee304cd9e4b58483d2ca
Kan-Ru Chen <kanru@kanru.info>
2026-09-05 21:16:48 +0900

    conversion: re-enable conversion/simple tests

--- src/conversion/simple.rs
+++ src/conversion/simple.rs
@@ -1,14 +1,13 @@
 use crate::{
     conversion::{Composition, ConversionEngine, Interval, Outcome},
-    dictionary::{LookupStrategy, StringTable},
-    lm::StaticDict,
+    dictionary::{CompositeDict, LookupStrategy, StringTable},
 };
 
 /// Simple engine does not perform any intelligent conversion.
 #[derive(Debug)]
 pub struct SimpleEngine {
     string_table: StringTable,
-    static_dict: StaticDict,
+    dict: CompositeDict,
 }
 
 impl SimpleEngine {
@@ -32,13 +31,13 @@ impl SimpleEngine {
                 });
             } else {
                 let phrase = self
-                    .static_dict
+                    .dict
                     .lookup(&[sym.to_syllable().unwrap()], LookupStrategy::Standard)
                     .first()
                     .cloned();
                 let phrase_str = phrase.map_or_else(
                     || sym.to_syllable().unwrap().to_string(),
-                    |wid| {
+                    |(wid, _)| {
                         self.string_table
                             .get_text(wid)
                             .unwrap_or("".into())
@@ -53,7 +52,18 @@ impl SimpleEngine {
                 })
             }
         }
-        // intervals.extend_from_slice(comp.selections());
+        intervals.extend(comp.selections().into_iter().filter_map(|s| {
+            if let Some(text) = self.string_table.get_text(s.wid) {
+                Some(Interval {
+                    start: s.start,
+                    end: s.end,
+                    is_phrase: true,
+                    text: text.into_boxed_str(),
+                })
+            } else {
+                None
+            }
+        }));
         intervals.sort_by_key(|int| int.start);
         vec![Outcome {
             intervals,
@@ -73,164 +83,107 @@ mod tests {
     use super::SimpleEngine;
     use crate::{
         conversion::{Composition, Interval, Outcome, Symbol},
-        dictionary::{Dictionary, TrieBuf},
+        dictionary::{CompositeDict, StringTable},
+        lm::StaticDict,
         syl,
+        user::{HistoryDict, UserDict},
         zhuyin::Bopomofo::*,
     };
 
-    fn test_dictionary() -> impl Dictionary {
-        TrieBuf::from([
-            (vec![syl![G, U, O, TONE2]], vec![("國", 1)]),
-            (vec![syl![M, I, EN, TONE2]], vec![("民", 1)]),
-            (vec![syl![D, A, TONE4]], vec![("大", 1)]),
-            (vec![syl![H, U, EI, TONE4]], vec![("會", 1)]),
-            (vec![syl![D, AI, TONE4]], vec![("代", 1)]),
-            (vec![syl![B, I, AU, TONE3]], vec![("表", 1), ("錶", 1)]),
-            (vec![syl![C, E, TONE4]], vec![("測", 18)]),
-            (vec![syl![SH, TONE4]], vec![("試", 18)]),
-            (
-                vec![syl![G, U, O, TONE2], syl![M, I, EN, TONE2]],
-                vec![("國民", 200)],
-            ),
-            (
-                vec![syl![D, A, TONE4], syl![H, U, EI, TONE4]],
-                vec![("大會", 200)],
-            ),
-            (
-                vec![syl![D, AI, TONE4], syl![B, I, AU, TONE3]],
-                vec![("代表", 200), ("戴錶", 100)],
-            ),
-            (vec![syl![X, I, EN]], vec![("心", 1)]),
-            (vec![syl![K, U, TONE4], syl![I, EN]], vec![("庫音", 300)]),
-            (
-                vec![syl![X, I, EN], syl![K, U, TONE4], syl![I, EN]],
-                vec![("新酷音", 200)],
-            ),
-            (
-                vec![syl![C, E, TONE4], syl![SH, TONE4], syl![I, TONE2]],
-                vec![("測試儀", 42)],
-            ),
-            (
-                vec![syl![C, E, TONE4], syl![SH, TONE4]],
-                vec![("測試", 9318)],
-            ),
-            (
-                vec![syl![I, TONE2], syl![X, I, A, TONE4]],
-                vec![("一下", 10576)],
-            ),
-            (vec![syl![X, I, A, TONE4]], vec![("下", 10576)]),
-            (vec![syl![H, A]], vec![("哈", 1)]),
-            (vec![syl![H, A], syl![H, A]], vec![("哈哈", 1)]),
-        ])
+    fn test_dictionary(string_table: StringTable) -> CompositeDict {
+        let user_dict = UserDict::new(string_table.clone());
+
+        user_dict.insert(&[syl![G, U, O, TONE2]], "國");
+        user_dict.insert(&[syl![M, I, EN, TONE2]], "民");
+        user_dict.insert(&[syl![D, A, TONE4]], "大");
+        user_dict.insert(&[syl![H, U, EI, TONE4]], "會");
+        user_dict.insert(&[syl![D, AI, TONE4]], "代");
+        user_dict.insert(&[syl![B, I, AU, TONE3]], "表");
+        user_dict.insert(&[syl![B, I, AU, TONE3]], "錶");
+        user_dict.insert(&[syl![G, U, O, TONE2], syl![M, I, EN, TONE2]], "國民");
+        user_dict.insert(&[syl![D, A, TONE4], syl![H, U, EI, TONE4]], "大會");
+        user_dict.insert(&[syl![D, AI, TONE4], syl![B, I, AU, TONE3]], "代表");
+        user_dict.insert(&[syl![D, AI, TONE4], syl![B, I, AU, TONE3]], "戴錶");
+        user_dict.insert(&[syl![X, I, EN]], "心");
+        user_dict.insert(&[syl![K, U, TONE4], syl![I, EN]], "庫音");
+        user_dict.insert(&[syl![X, I, EN], syl![K, U, TONE4], syl![I, EN]], "新酷音");
+        user_dict.insert(
+            &[syl![C, E, TONE4], syl![SH, TONE4], syl![I, TONE2]],
+            "測試儀",
+        );
+        user_dict.insert(&[syl![C, E, TONE4], syl![SH, TONE4]], "測試");
+        user_dict.insert(&[syl![I, TONE2], syl![X, I, A, TONE4]], "一下");
+        user_dict.insert(&[syl![X, I, A, TONE4]], "下");
+        user_dict.insert(&[syl![H, A]], "哈");
+        user_dict.insert(&[syl![H, A], syl![H, A]], "哈哈");
+
+        CompositeDict::new(
+            StaticDict::new(),
+            StaticDict::new(),
+            HistoryDict::new(string_table),
+            user_dict,
+        )
     }
 
-    //     #[test]
-    //     fn convert_empty_composition() {
-    //         let dict = test_dictionary();
-    //         let engine = SimpleEngine::new();
-    //         let composition = Composition::new();
-    //         assert_eq!(
-    //             vec![Outcome {
-    //                 intervals: vec![],
-    //                 log_prob: 0.0
-    //             }],
-    //             engine.convert(&dict, &composition)
-    //         );
-    //     }
-
-    //     // Some corrupted user dictionary may contain empty length syllables
-    //     #[test]
-    //     fn convert_zero_length_entry() {
-    //         let mut dict = test_dictionary();
-    //         dict.add_phrase(&[], ("", 0).into()).unwrap();
-    //         let engine = SimpleEngine::new();
-    //         let mut composition = Composition::new();
-    //         for sym in [
-    //             Symbol::from(syl![C, E, TONE4]),
-    //             Symbol::from(syl![SH, TONE4]),
-    //         ] {
-    //             composition.push(sym);
-    //         }
-    //         assert_eq!(
-    //             vec![Outcome {
-    //                 intervals: vec![
-    //                     Interval {
-    //                         start: 0,
-    //                         end: 1,
-    //                         is_phrase: true,
-    //                         text: "測".into()
-    //                     },
-    //                     Interval {
-    //                         start: 1,
-    //                         end: 2,
-    //                         is_phrase: true,
-    //                         text: "試".into()
-    //                     },
-    //                 ],
-    //                 log_prob: 0.0
-    //             }],
-    //             engine.convert(&dict, &composition)
-    //         );
-    //     }
-
-    //     #[test]
-    //     fn convert_simple_chinese_composition() {
-    //         let dict = test_dictionary();
-    //         let engine = SimpleEngine::new();
-    //         let mut composition = Composition::new();
-    //         for sym in [
-    //             Symbol::from(syl![G, U, O, TONE2]),
-    //             Symbol::from(syl![M, I, EN, TONE2]),
-    //             Symbol::from(syl![D, A, TONE4]),
-    //             Symbol::from(syl![H, U, EI, TONE4]),
-    //             Symbol::from(syl![D, AI, TONE4]),
-    //             Symbol::from(syl![B, I, AU, TONE3]),
-    //         ] {
-    //             composition.push(sym);
-    //         }
-    //         assert_eq!(
-    //             vec![Outcome {
-    //                 intervals: vec![
-    //                     Interval {
-    //                         start: 0,
-    //                         end: 1,
-    //                         is_phrase: true,
-    //                         text: "國".into()
-    //                     },
-    //                     Interval {
-    //                         start: 1,
-    //                         end: 2,
-    //                         is_phrase: true,
-    //                         text: "民".into()
-    //                     },
-    //                     Interval {
-    //                         start: 2,
-    //                         end: 3,
-    //                         is_phrase: true,
-    //                         text: "大".into()
-    //                     },
-    //                     Interval {
-    //                         start: 3,
-    //                         end: 4,
-    //                         is_phrase: true,
-    //                         text: "會".into()
-    //                     },
-    //                     Interval {
-    //                         start: 4,
-    //                         end: 5,
-    //                         is_phrase: true,
-    //                         text: "代".into()
-    //                     },
-    //                     Interval {
-    //                         start: 5,
-    //                         end: 6,
-    //                         is_phrase: true,
-    //                         text: "表".into()
-    //                     },
-    //                 ],
-    //                 log_prob: 0.0
-    //             }],
-    //             engine.convert(&dict, &composition)
-    //         );
-    //     }
+    #[test]
+    fn convert_simple_chinese_composition() {
+        let string_table = StringTable::new();
+        let dict = test_dictionary(string_table.clone());
+        let engine = SimpleEngine { string_table, dict };
+        let mut composition = Composition::new();
+        for sym in [
+            Symbol::from(syl![G, U, O, TONE2]),
+            Symbol::from(syl![M, I, EN, TONE2]),
+            Symbol::from(syl![D, A, TONE4]),
+            Symbol::from(syl![H, U, EI, TONE4]),
+            Symbol::from(syl![D, AI, TONE4]),
+            Symbol::from(syl![B, I, AU, TONE3]),
+        ] {
+            composition.push(sym);
+        }
+        assert_eq!(
+            vec![Outcome {
+                intervals: vec![
+                    Interval {
+                        start: 0,
+                        end: 1,
+                        is_phrase: true,
+                        text: "國".into()
+                    },
+                    Interval {
+                        start: 1,
+                        end: 2,
+                        is_phrase: true,
+                        text: "民".into()
+                    },
+                    Interval {
+                        start: 2,
+                        end: 3,
+                        is_phrase: true,
+                        text: "大".into()
+                    },
+                    Interval {
+                        start: 3,
+                        end: 4,
+                        is_phrase: true,
+                        text: "會".into()
+                    },
+                    Interval {
+                        start: 4,
+                        end: 5,
+                        is_phrase: true,
+                        text: "代".into()
+                    },
+                    Interval {
+                        start: 5,
+                        end: 6,
+                        is_phrase: true,
+                        text: "表".into()
+                    },
+                ],
+                cost: 0.0
+            }],
+            engine.convert(&composition)
+        );
+    }
 }
